de6acbbbafa6f6b04d3cf1ce08611a31a86eb793
Gus Hecht <gushecht@gmail.com>
2016-03-11 15:34:03 -0800

    finally have working version of merge_text

--- bin/merge_text.py
+++ bin/merge_text.py
@@ -1,19 +1,11 @@
 from __future__ import print_function, unicode_literals, division
 import io
-import bz2
-import logging
-from toolz import partition
 from os import path
-import os
 import re
 
 import spacy.en
-from preshed.counter import PreshCounter
-from spacy.tokens.doc import Doc
 
-from joblib import Parallel, delayed
 import plac
-import ujson
 
 
 LABELS = {
@@ -39,55 +31,24 @@ LABELS = {
 }
 
 
-def parallelize(func, iterator, n_jobs, extra):
-    extra = tuple(extra)
-    return Parallel(n_jobs=n_jobs)(delayed(func)(*(item + extra)) for item in iterator)
+# Creates an iterator that returns one line at a time to reduce memory use
+def line_iterator(in_file):
+    with open(in_file, 'r') as file_:
+        for line in file_:
+            yield unicode(line)
 
 
-def iter_comments(loc):
-    with bz2.BZ2File(loc) as file_:
-        for i, line in enumerate(file_):
-            yield ujson.loads(line)['body']
-
-
-pre_format_re = re.compile(r'^[\`\*\~]')
-post_format_re = re.compile(r'[\`\*\~]$')
-url_re = re.compile(r'\[([^]]+)\]\(%%URL\)')
-link_re = re.compile(r'\[([^]]+)\]\(https?://[^\)]+\)')
-def strip_meta(text):
-    text = link_re.sub(r'\1', text)
-    text = text.replace('&gt;', '>').replace('&lt;', '<')
-    text = pre_format_re.sub('', text)
-    text = post_format_re.sub('', text)
-    return text
-
-
-def load_and_transform(batch_id, in_loc, out_dir):
-    out_loc = path.join(out_dir, '%d.txt' % batch_id)
-    if path.exists(out_loc):
-        return None
-    print('Batch', batch_id)
-    nlp = spacy.en.English(parser=False, tagger=False, matcher=False, entity=False)
-    with io.open(out_loc, 'w', encoding='utf8') as out_file:
-        with io.open(in_loc, 'rb') as in_file:
-            for byte_string in Doc.read_bytes(in_file):
-                doc = Doc(nlp.vocab).from_bytes(byte_string)
-                doc.is_parsed = True
-                out_file.write(transform_doc(doc)) 
-
-
+# Takes a doc object from the pipe, puts it into the transformation function
+# and writes the output to file
 def parse_and_transform(batch_id, input_, out_dir):
     out_loc = path.join(out_dir, '%d.txt' % batch_id)
     if path.exists(out_loc):
         return None
-    print('Batch', batch_id)
-    nlp = spacy.en.English()
-    nlp.matcher = None
     with io.open(out_loc, 'w', encoding='utf8') as file_:
-        for text in input_:
-            file_.write(transform_doc(nlp(strip_meta(text))))
+        file_.write(transform_doc(input_))
 
 
+# Takes a doc object, constructs entities and adds tags, returning a string
 def transform_doc(doc):
     for ent in doc.ents:
         ent.merge(ent.root.tag_, ent.text, LABELS[ent.label_])
@@ -105,6 +66,7 @@ def transform_doc(doc):
         return ''
 
 
+# Takes a token object returns a properly formatted string representation of it
 def represent_word(word):
     if word.like_url:
         return '%%URL|X'
@@ -116,22 +78,20 @@ def represent_word(word):
 
 
 @plac.annotations(
-    in_loc=("Location of input file"),
-    out_dir=("Location of input file"),
-    n_workers=("Number of workers", "option", "n", int),
-    load_parses=("Load parses from binary", "flag", "b"),
+    in_file=('Location of input file'),
+    out_dir=('Path to output directory'),
+    batch_size=('Number of sequences of unicode objects to accumulate in buffer', 'option', 'b', int),
+    n_threads=('Number of processes to use while operating on unicode objects', 'option', 'p', int)
 )
-def main(in_loc, out_dir, n_workers=4, load_parses=False):
+def main(in_file, out_dir, batch_size=500, n_threads=2):
     if not path.exists(out_dir):
         path.join(out_dir)
-    if load_parses:
-        jobs = [path.join(in_loc, fn) for fn in os.listdir(in_loc)]
-        do_work = load_and_transform
     else:
-        jobs = partition(200000, iter_comments(in_loc))
-        do_work = parse_and_transform
-    parallelize(do_work, enumerate(jobs), n_workers, [out_dir])
- 
+        print('Loading spacy.en.English, this may take a while....')
+        nlp = spacy.en.English()
+        print('spacy.en.English loaded!')
+        for i, doc in enumerate(nlp.pipe(line_iterator(in_file), batch_size, n_threads)):
+            parse_and_transform(i, doc, out_dir)
 
 if __name__ == '__main__':
     plac.call(main)
---
+++ test.py
@@ -0,0 +1,143 @@
+# TO DO
+# Use my own data instead of Reddit
+# Optimize KMeans
+# Try ANNOY with different numbers of trees, different distance metrics
+# Slim down ANNOY implementation (i.e. reduce number of mapping dictionaries)
+#   by using get_nns_by_vector and get_item_vector functions
+
+import sense2vec
+import numpy as np
+from sklearn.cluster import KMeans, MiniBatchKMeans
+from annoy import AnnoyIndex
+from itertools import izip
+import pickle
+
+# For now, let's just use the data that comes with it
+model = sense2vec.load()
+
+# All lables for nouns
+LABELS = [
+    'ENT',
+    'PERSON',
+    'NORP',
+    'FAC',
+    'ORG',
+    'GPE',
+    'LOC',
+    'LAW',
+    'PRODUCT',
+    'EVENT',
+    'WORK_OF_ART',
+    'LANGUAGE',
+    'NOUN'
+]
+
+# Create dictionary to map words to vectors
+word_to_vect_dict = {}
+# Create dictionary to map words to AnnoyIndex's integer identifiers
+word_to_identifier_dict = {}
+identifier = 0
+for i, string in enumerate(model.strings):
+    # If the string is a noun, add it and its vector to the dictionary
+    if any(label in string for label in LABELS):
+        word_to_vect_dict[string] = model[string][1]
+        word_to_identifier_dict[string] = identifier
+        identifier += 1
+    # Just to track progress
+    if i % 10000 == 0:
+        print 'String #%s of %s --> %s' % \
+              (i, len(model.strings), (i / float(len(model.strings))))
+
+# Create an array of the vectors
+word_vectors = np.array(word_to_vect_dict.values())
+
+NUM_WORDS = len(word_vectors)
+
+# Create and fit a KMeans clustering model
+kmeans = KMeans(n_clusters=100,
+                n_jobs=-1)
+kmeans.fit(word_vectors)
+
+mini_kmeans = MiniBatchKMeans(n_clusters=20, verbose=True)
+mini_kmeans.fit(word_vectors)
+
+# Get the centroids of each cluster
+centroids = kmeans.cluster_centers_
+mini_centroids = mini_kmeans.cluster_centers_
+
+# F is this number of dimensions of the vectors, in this case 128 since that's
+# what sense2vec works with
+f = 128
+
+# Create an AnnoyIndex
+t = AnnoyIndex(f, metric='euclidean')
+
+# Add the word vectors to the AnnoyIndex
+for i, vector in izip(word_to_identifier_dict.values(),
+                      word_to_vect_dict.values()):
+    t.add_item(i, vector)
+
+# Create dictionary to map integer identifiers to centroid vectors
+identifier_to_centroid_vect_dict = {}
+for i in xrange(len(centroids)):
+    identifer = NUM_WORDS + i
+    identifier_to_centroid_vect_dict[identifer] = centroids[i]
+
+for i, vector in izip(identifier_to_centroid_vect_dict.keys(),
+                      identifier_to_centroid_vect_dict.values()):
+    t.add_item(i, vector)
+
+t.build(10)
+
+for i in xrange(8):
+    closest = t.get_nns_by_item(identifier_to_centroid_vect_dict.keys()[i], 20)
+
+    for key, value in word_to_identifier_dict.iteritems():
+        if value in closest:
+            print key
+
+    print '\n'
+
+
+##########
+
+# F is this number of dimensions of the vectors, in this case 128 since that's
+# what sense2vec works with
+f = 128
+
+# Create an AnnoyIndex
+t = AnnoyIndex(f, metric='euclidean')
+
+# Add the word vectors to the AnnoyIndex
+for i, vector in izip(word_to_identifier_dict.values(),
+                      word_to_vect_dict.values()):
+    t.add_item(i, vector)
+
+# Create dictionary to map integer identifiers to centroid vectors
+identifier_to_centroid_vect_dict = {}
+for i in xrange(len(mini_centroids)):
+    identifer = NUM_WORDS + i
+    identifier_to_centroid_vect_dict[identifer] = mini_centroids[i]
+
+for i, vector in izip(identifier_to_centroid_vect_dict.keys(),
+                      identifier_to_centroid_vect_dict.values()):
+    t.add_item(i, vector)
+
+t.build(10)
+
+for i in xrange(len(mini_centroids)):
+    closest = t.get_nns_by_item(identifier_to_centroid_vect_dict.keys()[i], 20)
+
+    for key, value in word_to_identifier_dict.iteritems():
+        if value in closest:
+            print key
+
+    print '\n'
+
+t.save('test.ann')
+
+with open('kmeans.pkl', 'wb') as f:
+    pickle.dump(kmeans, f)
+
+with open('mini_kmeans.pkl', 'wb') as f:
+    pickle.dump(mini_kmeans, f)
